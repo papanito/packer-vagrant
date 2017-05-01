372f794ef7530692f8182acfc7a9e8f17d885588
Adrian Wyssmann <adrian.wyssmann@roche.com>
2017-05-01 08:17:11 +0200

    Provisioning with script instead inline

--- packerfiles/scripts/ubuntu.sh
+++ packerfiles/scripts/ubuntu.sh
@@ -5,5 +5,5 @@ set -x
 echo "Additional configuration for ubuntu boxes"
 
 if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ]; then
-  echo "do $PACKER_BUILDER_TYPE specific provisioning
+  echo "do $PACKER_BUILDER_TYPE specific provisioning"
 fi
---
+++ packerfiles/scripts/windows10.ps1
@@ -0,0 +1,3 @@
+Write-Output "test1";
+Write-Host "test2";
+"test3";
