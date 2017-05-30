8535257ed7f83899aaaf5fa822bde386b059d277
Papanito <papanito@wyssmann.com>
2017-05-30 13:15:06 +0200

    Updates script to work properly and add "Hyper-V" specific fix to avoid black screen if image is imported to SCVMM

--- packerfiles/scripts/ubuntu.sh
+++ packerfiles/scripts/ubuntu.sh
@@ -4,6 +4,11 @@ set -x
 
 echo "Additional configuration for ubuntu boxes"
 
-if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ]; then
+if [ "$PACKER_BUILDER_TYPE" = "virtualbox-iso" ]; then
   echo "do $PACKER_BUILDER_TYPE specific provisioning"
+  
+  echo "Fix for Hyper-V black terminal: https://wellsie.net/p/395/"
+  echo 'vagrant' | sudo -S sed -i 's/quiet splash//g' /etc/default/grub
+  sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' /etc/default/grub
+  sudo update-grub
 fi
